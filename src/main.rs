f3c6f1432d9691cb8dba9b79fa2f51bb860fbb89
gsspdev <dev@gssp.io>
2024-01-24 03:52:15 -0800

    formatting changes

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,16 @@
 //   }
 // }
 impl Solution {
-    pub fn merge_two_lists(list1: Option<Box<ListNode>>, list2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    pub fn merge_two_lists(list1: Option<Box<ListNode>>, 
+                           list2: Option<Box<ListNode>>) 
+                               -> Option<Box<ListNode>> {
+
         match (list1, list2) {
             (Some(l1), None) => Some(l1),
             (None, Some(l2)) => Some(l2),
             (None, None)     => None,
             (Some(l1), Some(l2)) =>
+                
                 match l1.val <= l2.val {
                     true  => Some(Box::new(ListNode {
                         val: l1.val,
